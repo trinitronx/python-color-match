f1db0bab6c2bb8f010a7910a38407a11a9cc43c7
James Cuzella <james.cuzella@lyraphase.com>
2025-07-16 23:36:46 -0600

    Fix demo main() function

--- src/color_match/__main__.py
+++ src/color_match/__main__.py
@@ -1,6 +1,12 @@
-main():
-    print(f"This is only printed when running {__file__} directly, because the __name__ is {__name__}")
-    print(f"BSD Delta E: {calculate_delta_e("#eb0028", "#d70000")}")
+from .util.color import calculate_delta_e
 
-if __name__ == '__main__':
+
+def main():
+    print(
+        f"This is only printed when running {__file__} directly, because the __name__ is {__name__}"
+    )
+    print(f"BSD Delta E: {calculate_delta_e('#eb0028', '#d70000')}")
+
+
+if __name__ == "__main__":
     main()
